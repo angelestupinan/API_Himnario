8b0b66dea978bb079560a769edb40c92c61ce0c3
angelestupinan <angel200@nauta.cu>
2024-02-16 15:56:39 -0500

    added the nuguet packages needed, added the entity model Himnario and the IRepository interface from ardalis

---
+++ Application/Wrappers/Response.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Wrappers
+{
+    public class Response<T>
+    {
+        ///clase que se encarga de dar una respuesta uniforme
+        public bool Succeeded { get; set; }
+
+        public string Message { get; set; }
+
+        public List<string> Errors { get; set; }
+
+        public T Result { get; set; }
+
+        public Response() { }
+
+        public Response(T data, string message = null)
+        {
+            Succeeded = true;
+            Message = message;
+            Result = data;
+        }
+    }
+}
---
+++ Domain/Entities/Himno.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Himno
+    {
+
+        public int Id { get; set; }
+
+        public int Number { get; set; }
+
+        public string Name { get; set; }
+
+        public string Lyrics { get; set; }
+
+        public string Type { get; set; }
+
+        public string Clasification { get; set; }
+
+        public string Status { get; set; }
+
+    }
+}
