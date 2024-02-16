2ee0019376a43ea5d002fcdbe87257d9a64c5c13
angelestupinan <angel200@nauta.cu>
2024-02-16 14:02:42 -0500

    Agregar archivos de proyecto.

---
+++ Application/Exceptions/PipelineValidationException.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Exceptions
+{
+    public class PipelineValidationException : Exception
+    {
+        public List<string> Errors { get;}    
+
+        public PipelineValidationException() :base("Se ha producido uno o mas errores de validacion")
+        {
+
+            Errors = new List<string>();
+        }
+
+        public PipelineValidationException(IEnumerable<ValidationFailure> failures) : this()
+        {
+            foreach (var failure in failures)
+            {
+                Errors.Add(failure.ErrorMessage);
+            }
+        }
+      
+    }
+}
---
+++ Application/ServiceExtensions.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public static class ServiceExtensions
+    {
+        public  static void AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
+        }
+    }
+}
